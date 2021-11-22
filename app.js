084e07037345edd98ee59c2c9214be50dd636ccb
turanov97 <turanovbekzod116@gmail.com>
2021-11-22 15:29:12 +0500

    pricesss

--- app.js
+++ app.js
@@ -32,5 +32,37 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-
+/* services tabs */
  
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
+const tabsItem = document.querySelectorAll('.tabs__item')
+
+tabsBtn.forEach(onTabClick );
+
+function onTabClick(item) {
+   item.addEventListener('click', function () {
+      let currentBtn = item;
+      let tabId = currentBtn.getAttribute('data-tab');
+      let currentTab = document.querySelector(tabId);
+
+      if (!currentBtn.classList.contains('active')) {
+
+         tabsBtn.forEach(function (item) {
+            item.classList.remove('active')
+         })
+         tabsItem.forEach(function (item) {
+            item.classList.remove('active')
+         })
+         currentBtn.classList.add('active')
+         currentTab.classList.add('active')
+      }
+
+   })
+}
+
+
+document.querySelector('.tabs__nav-btn:nth-child(1)').click()
+
+
+
+
